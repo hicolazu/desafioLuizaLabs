98e86bf9196acc663fe6b631aaaa35e1f78804f6
Henrico Lazuroz <henrico.almeida@b2wdigital.com>
2022-06-11 17:42:45 -0300

    feat: Adding third feature: list unknow friends from friends

--- src/app.py
+++ src/app.py
@@ -4,6 +4,8 @@ import service.person_service as service
 
 from flask import Flask, Response, request
 
+from src.mapper.person_mapper import map_person_to_dict
+
 logging.basicConfig(level=logging.INFO, force=True)
 
 
@@ -14,7 +16,9 @@ def create_app():
     def get_all_people() -> Response:
         people = service.get_all_person()
 
-        return Response(response=json.dumps(people), status=200, mimetype='application/json')
+        people_dict = list(map(lambda x: map_person_to_dict(x), people))
+
+        return Response(response=json.dumps(people_dict), status=200, mimetype='application/json')
 
     @app.route('/friend_list', methods=['GET'])
     def get_friends_of_person() -> Response:
@@ -22,6 +26,19 @@ def create_app():
         name = query_parameters.get('name')
         friend_list = service.get_friend_list(name)
 
-        return Response(response=json.dumps(friend_list), status=200, mimetype='application/json')
+        friend_list_dict = list(map(lambda x: map_person_to_dict(x), friend_list))
+
+        return Response(response=json.dumps(friend_list_dict), status=200, mimetype='application/json')
+
+    @app.route('/non_friend_list', methods=['GET'])
+    def get_non_friend_list() -> Response:
+        query_parameters = request.args
+        name = query_parameters.get('name')
+        non_friend_list = service.get_non_friend_list(name)
+
+        non_friend_list_dict = list(map(lambda x: map_person_to_dict(x), non_friend_list))
+
+        return Response(response=json.dumps(non_friend_list_dict), status=200, mimetype='application/json')
+
 
     return app
--- src/service/person_service.py
+++ src/service/person_service.py
@@ -1,5 +1,6 @@
 import logging
 
+from src.model.person import Person
 from src.repository import person_repository
 from src.builder.graph_builder import build_graph
 from src.mapper.person_mapper import map_person_to_dict
@@ -7,15 +8,13 @@ from src.mapper.person_mapper import map_person_to_dict
 logging.basicConfig(level=logging.INFO, force=True)
 
 
-def get_all_person() -> [dict]:
+def get_all_person() -> [Person]:
     people = person_repository.find_all()
 
-    people_dict = list(map(lambda x: map_person_to_dict(x), people))
-
-    return people_dict
+    return people
 
 
-def get_friend_list(name: str) -> [dict]:
+def get_friend_list(name: str) -> [Person]:
     person = person_repository.find_by_name(name)
     graph = build_graph()
 
@@ -24,7 +23,23 @@ def get_friend_list(name: str) -> [dict]:
 
         people = person_repository.find_by_ids(person.friend_list)
 
-        people_dict = list(map(lambda x: map_person_to_dict(x), people))
+        return people
 
-        return people_dict
 
+def get_non_friend_list(name: str) -> [Person]:
+    non_friend_id_list = []
+    person = person_repository.find_by_name(name)
+
+    friend_list = get_friend_list(person.name)
+    friend_id_list = list(map(lambda x: x.id, friend_list))
+
+    for friend in friend_list:
+        friends_friend_list = get_friend_list(friend.name)
+
+        for friend_of_friend in friends_friend_list:
+            if (friend_of_friend.id not in friend_id_list) & (friend_of_friend.id is not person.id) & (friend_of_friend.id not in non_friend_id_list):
+                non_friend_id_list.append(friend_of_friend.id)
+
+    non_friend_list = person_repository.find_by_ids(non_friend_id_list)
+
+    return non_friend_list
